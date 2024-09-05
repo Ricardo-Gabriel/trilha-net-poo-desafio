670f28e1493f2a6b9edf6aaf46b65307f03ddeb6
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2024-09-05 13:52:26 -0300

    Implementaçoes das classes e metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+System.Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia("1234-567","Modelo 1","111111",128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+System.Console.WriteLine(" \n");
+
+System.Console.WriteLine("Smartphone Iphone");
+
+Smartphone iphone  = new Iphone("555-1234","Modelo 15","2222",64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
